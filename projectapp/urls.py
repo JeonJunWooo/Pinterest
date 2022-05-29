a631de10a9a5b55562ee1cbe83bee7eb13fd6094
SOOJIN <ksjin073018@gmail.com>
2022-05-29 18:21:35 +0900

    projectapp delete and update

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from projectapp.views import ProjectListView, ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectListView, ProjectCreateView, ProjectDetailView, ProjectDeleteView, ProjectUpdateView
+
 
 app_name = 'projectapp'
 
@@ -9,4 +10,6 @@ urlpatterns = [
 
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>', ProjectDeleteView.as_view(), name='delete'),
+    path('update/<int:pk>', ProjectUpdateView.as_view(), name='update'),
 ]
